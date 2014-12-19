b3a63d2a80b67eb705821afbb259f6bb33f94283
ZhangJintao <tao12345666333@163.com>
2014-12-19 19:17:21 +0800

    change directory

---
+++ scripts/downP.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+#coding=utf-8
+
+import re
+import random
+
+import requests
+
+def downloadspic(pic_id_list):
+    for pic_id in pic_id_list:       
+        for url in (
+            'http://www.pixiv.net/member_illust.php?mode=big&illust_id=%s'% pic_id,
+        ):
+            r = requests.get(url)
+            print url
+
+            if r.status_code == 200:
+                print 'Find the picture ...'
+                #print 'downloadspic=', pic_id
+                #with open('/home/tao/testp/'+str(pic_id), 'wb') as f:
+                #    for chunk in r.iter_content(chunk_size=1024):
+                #        if chunk:
+                #            f.write(chunk)
+                #            f.flush()
+                #    f.close()
+            else:
+                print 'The pictrue is not exsit'
+
+
+def main():
+    i =  range (44686855,44686880)
+    downloadspic(i)
+
+if __name__ == "__main__":
+    main()
